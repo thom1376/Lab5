f8a92eb8c0cee1fe89bc770d761763a2a6311a46
Logan Thomas <ltogan.2014@gmail.com>
2019-05-10 06:07:35 -0400

    Add button functionality in Scorekeeper project

--- Scorekeeper/app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ Scorekeeper/app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.android.scorekeeper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Member variables for holding the score
+    private int mScore1;
+    private int mScore2;
+    // Member variables for holding the score
+    private TextView mScoreText1;
+    private TextView mScoreText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Find the TextViews by ID
+        mScoreText1 = (TextView) findViewById(R.id.score_1);
+        mScoreText2 = (TextView) findViewById(R.id.score_2);
+    }
+
+    /**
+     * Method that handles the onClick of both the decrement buttons
+     *
+     * @param view The button view that was clicked
+     */
+    public void decreaseScore(View view) {
+        // Get the ID of the button that was clicked
+        int viewID = view.getId();
+        switch (viewID) {
+            //If it was on Team 1
+            case R.id.decreaseTeam1:
+                //Decrement the score and update the TextView
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            //If it was Team 2
+            case R.id.decreaseTeam2:
+                //Decrement the score and update the TextView
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    /**
+     * Method that handles the onClick of both the increment buttons
+     *
+     * @param view The button view that was clicked
+     */
+    public void increaseScore(View view) {
+        //Get the ID of the button that was clicked
+        int viewID = view.getId();
+        switch (viewID) {
+            //If it was on Team 1
+            case R.id.increaseTeam1:
+                //Increment the score and update the TextView
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            //If it was Team 2
+            case R.id.increaseTeam2:
+                //Increment the score and update the TextView
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
     }
 }
