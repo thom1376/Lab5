9ff1726aea9c47e7fbea31820ad8abe358f2a780
Logan Thomas <ltogan.2014@gmail.com>
2019-05-10 06:34:02 -0400

    Implement saved instance state in Scorekeeper project

--- Scorekeeper/app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ Scorekeeper/app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -14,14 +14,17 @@ public class MainActivity extends AppCompatActivity {
     private int mScore1;
     private int mScore2;
 
+    static final String STATE_SCORE_1 = "Team 1 Score";
+    static final String STATE_SCORE_2 = "Team 2 Score";
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         // Change the label of the menu based on the state of the app.
         int nightMode = AppCompatDelegate.getDefaultNightMode();
-        if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
             menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
-        } else{
+        } else {
             menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
         }
         return true;
@@ -30,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         //Check if the correct item was clicked
-        if(item.getItemId()==R.id.night_mode){
+        if (item.getItemId() == R.id.night_mode) {
             // Get the night mode state of the app.
             int nightMode = AppCompatDelegate.getDefaultNightMode();
             //Set the theme mode for the restarted activity
@@ -59,6 +62,15 @@ public class MainActivity extends AppCompatActivity {
         //Find the TextViews by ID
         mScoreText1 = (TextView) findViewById(R.id.score_1);
         mScoreText2 = (TextView) findViewById(R.id.score_2);
+
+        if (savedInstanceState != null) {
+            mScore1 = savedInstanceState.getInt(STATE_SCORE_1);
+            mScore2 = savedInstanceState.getInt(STATE_SCORE_2);
+
+            //Set the score text views
+            mScoreText1.setText(String.valueOf(mScore1));
+            mScoreText2.setText(String.valueOf(mScore2));
+        }
     }
 
     /**
@@ -105,5 +117,13 @@ public class MainActivity extends AppCompatActivity {
                 mScore2++;
                 mScoreText2.setText(String.valueOf(mScore2));
         }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // Save the scores.
+        outState.putInt(STATE_SCORE_1, mScore1);
+        outState.putInt(STATE_SCORE_2, mScore2);
+        super.onSaveInstanceState(outState);
     }
 }
