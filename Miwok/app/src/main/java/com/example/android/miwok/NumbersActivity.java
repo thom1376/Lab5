b53cf3aebe2de93932edc7609f59c922cd73dc1d
Logan Thomas <ltogan.2014@gmail.com>
2019-05-16 01:40:46 -0400

    Add audio playback to Miwok project

--- Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,12 +1,22 @@
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
+    private MediaPlayer mMediaPlayer;
+    private MediaPlayer.OnCompletionListener mOnCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,21 +24,60 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.word_list);
 
         // Create a list of words
-        ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word("one", "lutti", R.drawable.number_one));
-        words.add(new Word("two", "otiiko", R.drawable.number_two));
-        words.add(new Word("three", "tolookosu", R.drawable.number_three));
-        words.add(new Word("four", "oyyisa", R.drawable.number_four));
-        words.add(new Word("five", "massokka", R.drawable.number_five));
-        words.add(new Word("six", "temmokka", R.drawable.number_six));
-        words.add(new Word("seven", "kenekaku", R.drawable.number_seven));
-        words.add(new Word("eight", "kawinta", R.drawable.number_eight));
-        words.add(new Word("nine", "wo’e", R.drawable.number_nine));
-        words.add(new Word("ten", "na’aacha", R.drawable.number_ten));
+        final ArrayList<Word> words = new ArrayList<>();
+        words.add(new Word("one", "lutti", R.drawable.number_one,
+                R.raw.number_one));
+        words.add(new Word("two", "otiiko", R.drawable.number_two,
+                R.raw.number_two));
+        words.add(new Word("three", "tolookosu",
+                R.drawable.number_three, R.raw.number_three));
+        words.add(new Word("four", "oyyisa", R.drawable.number_four,
+                R.raw.number_four));
+        words.add(new Word("five", "massokka", R.drawable.number_five,
+                R.raw.number_five));
+        words.add(new Word("six", "temmokka", R.drawable.number_six,
+                R.raw.number_six));
+        words.add(new Word("seven", "kenekaku", R.drawable.number_seven,
+                R.raw.number_seven));
+        words.add(new Word("eight", "kawinta", R.drawable.number_eight,
+                R.raw.number_eight));
+        words.add(new Word("nine", "wo’e", R.drawable.number_nine,
+                R.raw.number_nine));
+        words.add(new Word("ten", "na’aacha", R.drawable.number_ten,
+                R.raw.number_ten));
 
         WordAdapter adapter = new WordAdapter(this, words, R.color.category_numbers);
 
         ListView listView = findViewById(R.id.list);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int audioResourceId = words.get(position).getAudioResourceId();
+                if (audioResourceId != Word.NO_RESOURCE_PROVIDED) {
+                    releaseMediaPlayer();
+                    mMediaPlayer = MediaPlayer.create(NumbersActivity.this, audioResourceId);
+                    mMediaPlayer.setOnCompletionListener(mOnCompletionListener);
+                    mMediaPlayer.start();
+                }
+            }
+        });
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
     }
 }
--- Miwok/app/src/main/java/com/example/android/miwok/Word.java
+++ Miwok/app/src/main/java/com/example/android/miwok/Word.java
@@ -3,19 +3,29 @@ package com.example.android.miwok;
 public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
-    private int mImageResourceId = NO_IMAGE_PROVIDED;
-    private static final int NO_IMAGE_PROVIDED = -1;
+    private int mImageResourceId = NO_RESOURCE_PROVIDED;
+    private int mAudioResourceId = NO_RESOURCE_PROVIDED;
+    public static final int NO_RESOURCE_PROVIDED = -1;
 
     public Word(String defaultTranslation, String miwokTranslation) {
         this.mDefaultTranslation = defaultTranslation;
         this.mMiwokTranslation = miwokTranslation;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
+        this.mDefaultTranslation = defaultTranslation;
+        this.mMiwokTranslation = miwokTranslation;
+        this.mAudioResourceId = audioResourceId;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId) {
         this.mDefaultTranslation = defaultTranslation;
         this.mMiwokTranslation = miwokTranslation;
         this.mImageResourceId = imageResourceId;
+        this.mAudioResourceId = audioResourceId;
     }
+
+
 
     public String getDefaultTranslation() {
         return mDefaultTranslation;
@@ -29,7 +39,11 @@ public class Word {
         return mImageResourceId;
     }
 
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+
     public boolean hasImage() {
-        return mImageResourceId != NO_IMAGE_PROVIDED;
+        return mImageResourceId != NO_RESOURCE_PROVIDED;
     }
 }
